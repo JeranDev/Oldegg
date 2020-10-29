bc5decfdb7025a51032b85e54d3d1bd792f20c91
JeranDev <jeranjb@gmail.com>
2020-10-28 18:50:38 -0600

    add mongoose and colors

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -23,5 +27,7 @@ const port = process.env.PORT || 5000
 
 app.listen(
   port,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${port}`.yellow.bold
+  )
 )
