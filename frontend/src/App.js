6ad7c77f0ff0545f25f72ce5ec3c1f1b17a41141
JeranDev <jeranjb@gmail.com>
2021-01-21 12:01:08 -0700

    add admin delete product functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
 
 const App = () => {
   return (
@@ -36,6 +37,7 @@ const App = () => {
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+          <Route path='/admin/productlist' component={ProductListScreen} />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
---
+++ frontend/src/screens/ProductListScreen.js
@@ -0,0 +1,104 @@
+//State
+import { useEffect } from 'react'
+//Redux
+import { useDispatch, useSelector } from 'react-redux'
+import { listProducts, deleteProduct } from '../actions/productsActions'
+//Components
+import { LinkContainer } from 'react-router-bootstrap'
+import { Table, Button, Row, Col } from 'react-bootstrap'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+
+const ProductListScreen = ({ history, match }) => {
+  const dispatch = useDispatch()
+
+  const { loading, error, products } = useSelector(state => state.productList)
+
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = useSelector(state => state.productDelete)
+
+  const { userInfo } = useSelector(state => state.userLogin)
+
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listProducts())
+    } else {
+      history.push('/login')
+    }
+  }, [dispatch, history, userInfo, successDelete])
+
+  const deleteHandler = id => {
+    if (window.confirm('Are you sure?')) {
+      dispatch(deleteProduct(id))
+    }
+  }
+
+  const createProductHandler = product => {
+    //CREATE PRODUCT
+  }
+
+  return (
+    <>
+      <Row className='align-items-center'>
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className='text-right'>
+          <Button className='my-3' onClick={createProductHandler}>
+            <i className='fas fa-plus'></i> Create Product
+          </Button>
+        </Col>
+      </Row>
+      {loadingDelete && <Loader />}
+      {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Table className='table-sm' striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>PRICE</th>
+              <th>CATEGORY</th>
+              <th>BRAND</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map(product => (
+              <tr key={product._id}>
+                <td>{product._id}</td>
+                <td>{product.name}</td>
+                <td>${product.price}</td>
+                <td>{product.category}</td>
+                <td>{product.brand}</td>
+                <td>
+                  <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                    <Button variant='light' className='btn-sm'>
+                      <i className='fas fa-edit'></i>
+                    </Button>
+                  </LinkContainer>
+                  <Button
+                    variant='danger'
+                    className='btn-sm'
+                    onClick={() => deleteHandler(product._id)}
+                  >
+                    <i className='fas fa-trash'></i>
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  )
+}
+
+export default ProductListScreen
